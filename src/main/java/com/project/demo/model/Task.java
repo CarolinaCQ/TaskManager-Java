8a885cef8fdedb4342af1e491ee1486575535f38
CarolinaCamilaQuevedo <CarolinaCQ>
2023-01-02 18:24:31 -0300

    feat : add model Task and changes model Project

--- src/main/java/com/project/demo/model/Task.java
+++ src/main/java/com/project/demo/model/Task.java
@@ -19,40 +19,51 @@ import static jakarta.persistence.GenerationType.IDENTITY;
 @Entity
 @Setter
 @Getter
-@Table(name = "projects")
-@SQLDelete(sql = "UPDATE projects SET deleted = true WHERE id = ?")
+@Table(name = "tasks")
+@SQLDelete(sql = "UPDATE tasks SET deleted = true WHERE id = ?")
 @Where(clause = "deleted = false")
-public class Project {
+public class Task {
 
     @Id
     @GeneratedValue(strategy = IDENTITY)
     @Column(name = "id")
-    private Long id;
+    Long id;
 
     @NotNull
     @Column(name = "title")
-    private String title;
+    String title;
 
     @Column(name = "description")
-    private String description;
+    String description;
+
+    @NotNull
+    @Column(name = "duration")
+    Double duration;
+
+    @Column(name = "finish_date")
+    LocalDateTime finishDate;
 
     @CreationTimestamp
     @Column(name = "creation_date")
-    private LocalDateTime creationDate;
+    LocalDateTime creationDate;
 
     @UpdateTimestamp
     @Column(name = "update_date")
-    private LocalDateTime updateDate;
+    LocalDateTime updateDate;
 
     @Column(name = "deleted")
-    private Boolean deleted;
+    Boolean deleted;
 
-    @OneToMany(mappedBy = "project")
-    @JsonIgnoreProperties("project")
-    private List<Task> tasks;
+    @Column(name = "condition")
+    private Condition condition;
 
-    /*@ManyToOne
-    @JsonIgnoreProperties("projects")
-    private User user;*/
+    /*@OneToMany
+    @JoinTable(name = "subtasks",
+            joinColumns = @JoinColumn(name = "id"))
+    private List<Subtask> subtasks;*/
 
+    @ManyToOne(fetch = EAGER)
+    @JoinColumn(name = "id")
+    @JsonIgnoreProperties("tasks")
+    private Project project;
 }
