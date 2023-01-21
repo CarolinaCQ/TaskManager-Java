f9a22783e73c6b87e4cb8ff3082f003538ae1a3f
CarolinaCamilaQuevedo <CarolinaCQ>
2023-01-21 18:49:20 -0300

    feat: add repository, service and controller condition

---
+++ src/main/java/com/project/demo/dto/ConditionDto.java
@@ -0,0 +1,16 @@
+package com.project.demo.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Setter
+@Getter
+@AllArgsConstructor
+@NoArgsConstructor
+public class ConditionDto {
+
+    private String title;
+    private String description;
+}
