ce995c324fa32e63ecc22e3f69da84aa969011e6
CarolinaCamilaQuevedo <CarolinaCQ>
2023-01-23 16:17:14 -0300

    feat: add anotation repository

--- src/main/java/com/project/demo/repository/SubtaskRepository.java
+++ src/main/java/com/project/demo/repository/SubtaskRepository.java
@@ -2,6 +2,8 @@ package com.project.demo.repository;
 
 import com.project.demo.model.Subtask;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface SubtaskRepository extends JpaRepository<Subtask, Long> {
 }
